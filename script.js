6f9b1b4b25f698ff6777fe1563bffac121f786dd
flormcalvo <flor.calvo89@gmail.com>
2020-01-30 17:41:49 +1100

    Working Copy

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ class App {
     this.author = "";
     this.pages = 0;
 
-    this.$body = document.querySelector("body");
     this.$form = document.querySelector("form");
     this.$newButton = document.querySelector(".new-book-button");
     this.$modal = document.querySelector(".modal");
@@ -18,9 +17,6 @@ class App {
     this.addEventListeners();
   }
   addEventListeners() {
-    this.$body.addEventListener("click", event => {
-      this.deleteBook(event);
-    });
     this.$newButton.addEventListener("click", event => {
       this.openModal();
     });
@@ -53,8 +49,7 @@ class App {
     const newBook = {
       title,
       author,
-      pages,
-      id: this.books.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1 //If length of array is greater than 0, I'll get the ID of the previous element and add one.
+      pages
     };
     this.books = [...this.books, newBook];
     this.displayCard();
@@ -64,7 +59,7 @@ class App {
     this.$books.innerHTML = this.books.map(
       book =>
         `
-      <div class="book-card" data-id=${book.id}>
+      <div class="book-card">
         <div class="book-info">
           <p class="book-title">${book.title}</p>
           <p class="book-author">Written by ${book.author}</p>
@@ -73,16 +68,12 @@ class App {
         </div>
         <div class="toolbar-container">
           <div class="toolbar">
-            <button class="delete-book-button"><img src="https://icon.now.sh/delete"></button>
+            <button class="close-card-button"><img class="toolbar-delete" src="https://icon.now.sh/delete"></button>
           </div>
         </div>
       </div>
       `
     );
-  }
-  deleteBook(event) {
-    const selectedBook = event.target.closest(".book-card");
-    console.log(selectedBook);
   }
 }
 
