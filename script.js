228b1ced46b79ee970ae846a6f053e2c6a6db355
flormcalvo <flor.calvo89@gmail.com>
2020-01-31 16:27:15 +1100

    Fixed persistance issues

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 class App {
   constructor() {
     this.books = [];
-
     this.$form = document.querySelector("form");
     this.$newButton = document.querySelector(".new-book-button");
     this.$modal = document.querySelector(".modal");
@@ -11,9 +10,9 @@ class App {
     this.$bookPages = document.querySelector(".modal-pages");
     this.$books = document.querySelector(".books");
     this.$body = document.querySelector("body");
-
     this.addEventListeners();
   }
+
   addEventListeners() {
     this.$body.addEventListener("click", event => {
       this.deleteBook(event);
@@ -38,36 +37,41 @@ class App {
       }
     });
   }
+
   openModal() {
     this.$modal.classList.add("open-modal");
   }
+
   closeModal() {
     this.$modal.classList.remove("open-modal");
     this.$bookTitle.value = "";
     this.$bookAuthor.value = "";
     this.$bookPages.value = "";
   }
+
   addBook({ title, author, pages }) {
     const newBook = {
       title,
       author,
       pages,
+      status: "Unread",
       id: this.books.length > 0 ? this.books[this.books.length - 1].id + 1 : 1
     };
     this.books = [...this.books, newBook];
     this.displayCard();
     this.closeModal();
   }
+
   displayCard() {
     this.$books.innerHTML = this.books.map(
-      book =>
-        `
+      book => `
       <div class="book-card" data-id="${book.id}">
         <div class="book-info">
           <p class="book-title">${book.title}</p>
           <p class="book-author">Written by ${book.author}</p>
           <p class="book-pages">${book.pages} pages.</p>
-          <button class="read-status-button unread-status">Unread</button>
+          <button class="read-status-button ${book.status == "Unread" &&
+            "unread-status"}">${book.status}</button>
         </div>
         <div class="toolbar-container">
           <div class="toolbar">
@@ -78,6 +82,7 @@ class App {
       `
     );
   }
+
   deleteBook(event) {
     const selectedElementMatches = event.target.matches(".toolbar-delete");
     if (!selectedElementMatches) return;
@@ -85,16 +90,24 @@ class App {
     this.books = this.books.filter(book => book.id != $bookToDelete.dataset.id);
     this.displayCard();
   }
+
   readStatus(event) {
     const selectedElementMatches = event.target.matches(".read-status-button");
     const $clickedButton = event.target;
     if (!selectedElementMatches) return;
-    $clickedButton.classList.toggle("unread-status");
-    if (event.target.matches(".unread-status")) {
-      $clickedButton.innerHTML = "Unread";
-    } else {
-      $clickedButton.innerHTML = "Read";
-    }
+    const $bookToUpdate = event.target.closest(".book-card");
+    this.books.map(book => {
+      if (book.id === Number($bookToUpdate.dataset.id)) {
+        if (book.status === "Read") {
+          book.status = "Unread";
+          $clickedButton.classList.add("unread-status");
+        } else {
+          book.status = "Read";
+          $clickedButton.classList.remove("unread-status");
+        }
+      }
+    });
+    this.displayCard();
   }
 }
 
